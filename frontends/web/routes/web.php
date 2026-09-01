3a6445a14cc3e78928a2d471314d1ffa52ef62b0
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-01 11:31:08 +0300

    feat(web): add payroll routes

--- frontends/web/routes/web.php
+++ frontends/web/routes/web.php
@@ -13,3 +13,9 @@ $app->router->post('/companies', [CompanyController::class, 'create'], ['require
 $app->router->get('/employees', [EmployeeController::class, 'index'], ['require_auth']);
 $app->router->get('/employees/create', [EmployeeController::class, 'showCreate'], ['require_auth']);
 $app->router->post('/employees', [EmployeeController::class, 'create'], ['require_auth']);
+
+$app->router->get('/payroll', [PayrollController::class, 'index'], ['require_auth']);
+$app->router->get('/payroll/create', [PayrollController::class, 'showCreate'], ['require_auth']);
+$app->router->post('/payroll', [PayrollController::class, 'create'], ['require_auth']);
+$app->router->get('/payroll/{run}', [PayrollController::class, 'show'], ['require_auth']);
+$app->router->post('/payroll/action', [PayrollController::class, 'action'], ['require_auth']);
